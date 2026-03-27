05b2261d3af11cff5e8a9c0663416fa0a52a3533
黄二哩个黄 <hh1911391294@163.com>
2026-03-28 01:11:29 +0800

    仍在编写

--- src/include/Driver/Video/Window.asm
+++ src/include/Driver/Video/Window.asm
@@ -20,7 +20,7 @@ create_win:         ;创建一个窗口,esi指向窗口属性
     pushad
         mov edi,win_chain_buffer
         check_win_chain:     
-            cmp [edi],byte 0FDh                 ;检查是否是有效表项
+            cmp [edi+1],byte 0FDh               ;检查是否是有效表项
                 jne create_chain                ;不是则在当前位置创建表
             cmp [edi+win_chain.closed],byte 1   ;检查是否是已关闭的窗口
                 je create_chain                 ;是则在当前位置创建表
@@ -31,13 +31,20 @@ create_win:         ;创建一个窗口,esi指向窗口属性
             mov [edi],word 0FD01h
             mov [edi+win_chain.closed], byte 0
             mov [edi+win_chain.visible], byte 1
-            mov ecx,[esi+win_attr.x] 
-            mov [edi+win_chain.x] ,ecx
-            mov ecx,[esi+win_attr.y]
-            mov [edi+win_chain.y] ,ecx
-            mov [edi+win_chain.attr] ,edx
+            mov cx,[esi+win_attr.x] 
+            mov [edi+win_chain.x] ,cx
+            mov cx,[esi+win_attr.y]
+            mov [edi+win_chain.y] ,cx
+            mov [edi+win_chain.attr] ,esi
             mov [edi+win_chain.next_win] ,dword 0
-            
-            
+            mov ebx,win_chain_buffer
+            cc_find_final:
+                cmp [ebx+win_chain.next_win], dword 0       ;确定是否是最后一个表项
+                je ccff_found                                        
+                mov ebx,[ebx+win_chain.next_win]            ;不是则跳到下一个表项
+            jmp cc_find_final
+            ccff_found:
+                mov [edi+win_chain.pre_win] ,ebx                                 ;将其的上一个窗口正确指向
+                mov [ebx+win_chain.next_win], edi             ;将上一个窗口的下一个窗口指向其
     popad
 ret
