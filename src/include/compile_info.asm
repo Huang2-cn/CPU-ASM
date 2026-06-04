7aedd7205bc3b7dc41d722b82629aa4ba5a854b4
黄二哩个黄 <hh1911391294@163.com>
2026-06-04 12:39:29 +0800

    -

--- src/include/compile_info.asm
+++ src/include/compile_info.asm
@@ -1,6 +1,6 @@
 compile_info:
 db "Compiled on:"
-db "2026/5/30",20h
-db "at 13:20  VER "
-db "3.0.141",0ah,0dh
+db "2026/6/4",20h
+db "at 12:38  VER "
+db "3.0.143",0ah,0dh
 db "Copyright (c) Huang2.cn" ,0ah,0dh,0
